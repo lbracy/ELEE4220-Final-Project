e0ff777c173c9cc8b3553f01e7d2dfcfdd71010a
Nektaria Karagiannis <91200279+nikkikaragi@users.noreply.github.com>
2026-04-12 14:55:41 -0400

    basic feedback

--- code/MagneticLevitator/src/main.cpp
+++ code/MagneticLevitator/src/main.cpp
@@ -3,44 +3,84 @@
 #define I_COIL 35
 #define V_HALL 34
 #define PWM_PIN 25
-// ESP32 PWM settings
-#define PWM_FREQ 10000
-#define PWM_RESOLUTION 8   // 8-bit: duty from 0 to 255
+#define SHUNT_PIN 26
+
+#define OPAMP_GAIN 0.77f // gain is 7.7 based on measuring resistors 
+#define Kp_inner 10.0f  // dummy vars
+#define Ki_inner 10.0f  // dummy vars
 
 float hall_voltage = 0.0;
-float coil_current = 0.0;
+int shunt_raw = 0;
+
+// inner loop constants
+int duty = 0; 
+float i_ref = 0.0f;
+float i_meas = 0.0f;
+float e_i = 0.0f;
 
 float readHallSensor();
+
+int inner_feedback_loop(float i, float i_ref) {
+  static float integral_i = 0.0f;
+  const float dt = 0.25f;   // 250 ms loop time
+
+  float error = i_ref - i;
+
+  // integrate error
+  integral_i += error * dt;
+
+  // PI control
+  float u = Kp_inner * error + Ki_inner * integral_i;
+
+  // clamp to PWM limits
+  if (u > 255.0f) {
+    u = 255.0f;
+  }
+  if (u < 0.0f) {
+    u = 0.0f;
+  }
+
+  return (int)u;
+}
 
 void setup() {
   Serial.begin(115200);
 
-  // Set up PWM on ESP32
   pinMode(PWM_PIN, OUTPUT);
   analogWriteFrequency(10000);   // 10 kHz
+
+  pinMode(SHUNT_PIN, INPUT);
+  pinMode(V_HALL, INPUT);
+  analogReadResolution(12);
 }
 
 void loop() {
-  //hall_voltage = readHallSensor();
-  //Serial.print("Hall Sensor Voltage: ");  
-  //Serial.println(hall_voltage);
-  delay(100); 
 
-  // pwm stuff
-  // for (int percent = 10; percent <= 100; percent += 10) {
-  //   int duty = (percent * 255) / 100;   // convert percent to 8-bit duty
+  i_ref = 0.4f;
 
-  //   analogWrite(PWM_PIN, 255);
-  //   Serial.print("PWM Percent: ");
-  //   Serial.print(percent);
-  //   Serial.print("% | Duty: ");
-  //   Serial.println(duty);
+  // int shuntRaw = analogReadMilliVolts(SHUNT_PIN);
+  // float v_out = shuntRaw / 1000.0f;
+  // float i_meas = v_out / OPAMP_GAIN;
 
-  //   delay(1000);
-  // }
-  Serial.println("Ramp complete. Turning PWM off.");
-  analogWrite(PWM_PIN, 255);
-  delay(3000);
+  int shuntRaw = analogRead(SHUNT_PIN);
+  float v_out = (shuntRaw / 4095.0f) * 3.3f;
+  i_meas = v_out / OPAMP_GAIN;
+
+  duty = inner_feedback_loop(i_meas, i_ref);
+  analogWrite(PWM_PIN, duty);
+
+  Serial.print("Duty: ");
+  Serial.print(duty);
+  Serial.print(" | Raw ADC Shunt: ");
+  Serial.print(shuntRaw);
+  Serial.print(" | Coil Current: ");
+  Serial.print(i_meas, 3);
+  Serial.print(" A");
+  Serial.print(" | Shunt Voltage: ");
+  Serial.print(v_out, 3);
+  Serial.println(" V");
+
+  delay(25);
 }
 
 float readHallSensor() {
